b9b66ddc63c5381836ace9fc4806e30bd5d1a4a0
gourav-gaba <gouravgaba2006@gmail.com>
2026-07-01 14:52:14 +0530

    media pipe model connected and logging of landmarks done

--- src/components/posedet.jsx
+++ src/components/posedet.jsx
@@ -1,13 +1,53 @@
 import { useEffect, useRef, useState } from "react";
+import { FilesetResolver, PoseLandmarker } from "@mediapipe/tasks-vision";
 
 export default function PoseDetection() {
 
     const videoRef = useRef(null);
     const [isCameraOn, setIsCameraOn] = useState(false);
     const streamRef = useRef(null);
+    const poseLandmarkerRef = useRef(null);
 
+    async function CreatePoseLandmarker() {
+        if (poseLandmarkerRef.current) {
+            return;
+        }
+        const vision = await FilesetResolver.forVisionTasks(
+            "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm"
+        );
+        poseLandmarkerRef.current = await PoseLandmarker.createFromOptions(
+            vision,
+            {
+                baseOptions: {
+                    modelAssetPath:
+                        "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/latest/pose_landmarker_lite.task"
+                },
+
+                runningMode: "VIDEO",
+
+                numPoses: 1
+            }
+        );
+    }
+    function detectpose() {
+        if (!poseLandmarkerRef.current) return;
+        if (videoRef.current.readyState < HTMLMediaElement.HAVE_CURRENT_DATA) {
+            requestAnimationFrame(detectpose);
+            return;
+        }
+        const results = poseLandmarkerRef.current.detectForVideo(
+            videoRef.current, performance.now()
+        );
+        if (results.landmarks.length > 0) {
+
+            console.log(results.landmarks[0]);
+
+        }
+        requestAnimationFrame(detectpose);
+
+    }
     async function startCamera() {
-
+        await CreatePoseLandmarker();
         const stream = await navigator.mediaDevices.getUserMedia({
             video: true
         });
@@ -15,11 +55,15 @@ export default function PoseDetection() {
         streamRef.current = stream;
 
         videoRef.current.srcObject = stream;
+        videoRef.current.onloadeddata = () => {
+            detectpose();
+        }
 
         setIsCameraOn(true);
+
     }
     async function stopCamera() {
-        streamRef.current.getTracks().forEach(track => track.stop());
+        if (streamRef.current) { streamRef.current.getTracks().forEach(track => track.stop()); }
 
         videoRef.current.srcObject = null;
 
