39e07a84eebae80561de1de1ad7665a9ddf178ca
gourav-gaba <gouravgaba2006@gmail.com>
2026-07-01 17:23:30 +0530

    now all 33 points on a body are shown not connected to form skeleton

--- src/components/posedet.jsx
+++ src/components/posedet.jsx
@@ -7,6 +7,8 @@ export default function PoseDetection() {
     const [isCameraOn, setIsCameraOn] = useState(false);
     const streamRef = useRef(null);
     const poseLandmarkerRef = useRef(null);
+    const canvasRef = useRef(null);
+
 
     async function CreatePoseLandmarker() {
         if (poseLandmarkerRef.current) {
@@ -29,6 +31,40 @@ export default function PoseDetection() {
             }
         );
     }
+    function drawpose(landmarks) {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+
+        canvas.width = videoRef.current.videoWidth;
+        canvas.height = videoRef.current.videoHeight;
+
+        ctx.clearRect(
+            0, 0, canvas.width, canvas.height
+        );
+
+        for (const landmark of landmarks) {
+            const x = landmark.x * canvas.width;
+            const y = landmark.y * canvas.height;
+            ctx.beginPath();
+            ctx.arc(
+                x,
+                y,
+                5,
+                0,
+                Math.PI * 2
+            );
+            ctx.fill();
+        }
+        // const nose = results.landmarks[0][0];
+        // const x = nose.x * canvas.width;
+        // const y = nose.y * canvas.height;
+
+        // ctx.beginPath();
+        // ctx.arc(
+        //     x, y, 5, 0, Math.PI * 2
+        // );
+        // ctx.fill();
+    }
     function detectpose() {
         if (!poseLandmarkerRef.current) return;
         if (videoRef.current.readyState < HTMLMediaElement.HAVE_CURRENT_DATA) {
@@ -41,8 +77,10 @@ export default function PoseDetection() {
         if (results.landmarks.length > 0) {
 
             console.log(results.landmarks[0]);
+            drawpose(results.landmarks[0]) //ye [0] one person ko dikha rha hai
 
         }
+
         requestAnimationFrame(detectpose);
 
     }
@@ -87,6 +125,9 @@ export default function PoseDetection() {
                     playsInline
                     className="pose-video-feed"
                 />
+                <canvas
+                    ref={canvasRef}
+                    className="pose-canvas" />
                 <div className="pose-video-overlay-glow"></div>
             </div>
         </div>
